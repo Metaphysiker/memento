86d1bdc6d1e2fa9083662419eedc781dda022056
Metaphysiker <s.raess@me.com>
2018-10-10 23:07:26 +0200

    added username to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   after_action :track_action
 
   protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 
   def track_action
     ahoy.track "Ran action", request.path_parameters
---
+++ spec/features/login_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "login", :type => :feature do
+  it "displays the user's username after successful login" do
+    login
+  end
+end
+
+def login
+  user = User.create!(:username => Faker::Internet.username, :email => Faker::Internet.email, :password => "secret")
+  visit "/users/sign_in"
+  fill_in "Login", :with => user.username
+  fill_in "Passwort", :with => "secret"
+  click_button "Log in"
+
+  expect(page).to have_selector(".navbar-brand", :text => user.username)
+end
