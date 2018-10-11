d79053cfbcf4235f50b61a23a4a4067434401ca3
Metaphysiker <s.raess@me.com>
2018-10-11 12:04:15 +0200

    user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is not valid without email" do
+    user = User.new(username: Faker::Internet.username, password: "secret")
+
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without username" do
+    user = User.new(email: Faker::Internet.email, password: "secret")
+
+    expect(user).to_not be_valid
+  end
+
+  it "is valid with username and email" do
+    user = User.new(email: Faker::Internet.email, username: Faker::Internet.username, password: "secret")
+
+    expect(user).to be_valid
+  end
+
 end
