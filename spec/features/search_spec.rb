d8b089b48cad373b401a2857726f48e73c3d44da
Metaphysiker <s.raess@me.com>
2018-10-14 12:12:56 +0200

    finished working on address

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -57,6 +57,9 @@ RSpec.describe "search", :type => :feature do
     expect(page).to_not have_content(person3.description)
     expect(page).to_not have_content(person3.phone)
 
+    sleep 2
+    page.save_screenshot('search3-expect1.png')
+
   end
 
   it "searches among 3 people and expects 3 results" do
