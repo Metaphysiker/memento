67e494780c493649c48ba352d2a6d8a3d5c9f9fa
Metaphysiker <s.raess@me.com>
2018-10-17 14:47:09 +0200

    working on institutions

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,4 +1,7 @@
 class Institution < ApplicationRecord
+  after_create :create_address
+
+  audited
 
   has_one :address, as: :addressable
 
@@ -6,4 +9,14 @@ class Institution < ApplicationRecord
   has_many :people, :through => :affiliations
 
   validates :name, presence: :true, uniqueness: :true
+
+
+  def create_address
+
+    Address.create(
+      addressable_id: self.id,
+      addressable_type: Institution,
+      company: self.name,
+    )
+  end
 end
