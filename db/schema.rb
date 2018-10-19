765da22c8dd400a463476209f891f6e2796c0efb
Metaphysiker <s.raess@me.com>
2018-10-19 17:50:10 +0200

    added note

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_104149) do
+ActiveRecord::Schema.define(version: 2018_10_19_151648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,15 @@ ActiveRecord::Schema.define(version: 2018_10_17_104149) do
     t.integer "philosophie_society_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "description"
+    t.string "noteable_type"
+    t.bigint "noteable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["noteable_type", "noteable_id"], name: "index_notes_on_noteable_type_and_noteable_id"
   end
 
   create_table "people", force: :cascade do |t|
