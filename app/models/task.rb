2acc527d6d4ddaf0e944292304730c148ce7476b
Metaphysiker <s.raess@me.com>
2018-10-21 11:37:39 +0200

    problem with search on overview

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,8 @@ class Task < ApplicationRecord
 
   belongs_to :taskable, polymorphic: true
 
+  scope :search_records_ilike, ->(search_term) { where("description ILIKE ? OR status ILIKE ?", search_term, search_term) }
+
   def self.statuses
     ['noch nicht angefangen', 'angefangen', 'bald abgeschlossen', 'abgeschlossen']
   end
