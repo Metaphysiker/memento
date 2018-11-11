e57e7d3b7a0efd6908d76fa632558f531fe2550a
Metaphysiker <s.raess@me.com>
2018-11-12 00:24:55 +0100

    sponsor added in but problem remains

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,7 +70,7 @@ institutions_functionality_tags.each do |tag|
 end
 
 institutions_target_groups_tags = ["Philosophisches Institut", "Kooperationspartner","SPG/SAGW", "Charles Hummel Stiftung",
-        "Stiftung", "Philosophischer Verein", "Verlag",
+        "Stiftung", "Philosophischer Verein", "Verlag", "Sponsor",
       "Verein", "öffentliche Institution", "Unternehmen", "Medienkontakt"]
 
 institutions_target_groups_tags.each do |tag|
