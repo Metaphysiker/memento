1911de939eda4ba9b0128d7ed236f399a96de083
Metaphysiker <s.raess@me.com>
2018-11-21 23:13:27 +0100

    group has many people

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,7 @@ class Group < ApplicationRecord
   audited
 
   has_many :group_people
-  has_many :groups, :through => :group_people
+  has_many :people, :through => :group_people
 
   scope :search_records_ilike, ->(search_term) { where("name ILIKE ? OR description ILIKE ?", search_term, search_term) }
 end
