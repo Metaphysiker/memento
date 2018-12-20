c9fce64763335c3c6c3fd7aa857ddf91b6039a83
Metaphysiker <s.raess@me.com>
2018-12-20 09:36:53 +0100

    added specs for groups and projects

---
+++ spec/features/groups_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "groups", :type => :feature do
+
+  before(:each) do
+    first_user = User.create!(:username => Faker::Internet.username, :role => "admin", :email => Faker::Internet.email, :password => "secret")
+    login_with(first_user)
+  end
+
+  it "creates a group" do
+    person = Person.create(
+      email: Faker::Internet.email,
+      firstname: Faker::Name.first_name,
+      lastname: Faker::Name.last_name,
+      description: Faker::Lorem.paragraph,
+      phone: Faker::PhoneNumber.cell_phone,
+      gender: Person.genders.sample
+    )
+
+    visit "/people/#{person.id}"
+    expect(page).to have_content(person.email)
+  end
+end
+
+def login_with(user)
+  visit "/users/sign_in"
+  fill_in "Login", :with => user.username
+  fill_in "Passwort", :with => "secret"
+  click_button "Log in"
+
+  expect(page).to have_selector(".navbar-brand", :text => user.username)
+end
