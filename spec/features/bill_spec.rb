20855981a090030c89483c7f2410e688224389b6
Metaphysiker <s.raess@me.com>
2019-07-28 16:34:36 +0200

    working on bill spec

---
+++ spec/features/bill_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe "bill", :type => :feature do
+
+  before(:each) do
+    first_user = User.create!(:username => Faker::Internet.username, :role => "admin", :email => Faker::Internet.email, :password => "secret")
+    login_with(first_user)
+  end
+
+  it "creates a bill" do
+
+    person = Person.create(
+      email: Faker::Internet.unique.email,
+      firstname: Faker::Name.unique.first_name,
+      lastname: Faker::Name.unique.last_name,
+      description: Faker::Lorem.unique.paragraph,
+      phone: Faker::PhoneNumber.unique.cell_phone,
+      gender: Person.genders.sample
+    )
+
+    person.address.update(
+      line1: Faker::Address.community,
+      line2: Faker::HarryPotter.quote,
+      line3: Faker::HarryPotter.character,
+      line4: Faker::GreekPhilosophers.name,
+      line5: Faker::GreekPhilosophers.quote,
+
+      street: Faker::Address.street_address,
+      plz: Faker::Address.zip_code,
+      location: Faker::Address.city,
+    )
+
+    visit "/people/#{person.id}"
+
+    within(".person-#{person.id}") do
+      click_link "Rechnung erstellen"
+    end
+
+  end
+
+
+end
+
+def login_with(user)
+  visit "/users/sign_in"
+  fill_in "Login", :with => user.username
+  fill_in "Passwort", :with => "secret"
+  click_button "Log in"
+
+  expect(page).to have_selector(".navbar-brand", :text => user.username)
+end
