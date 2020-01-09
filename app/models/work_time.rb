6a4e5c72e9fa75013449f91163f99646963b598a
Metaphysiker <s.raess@me.com>
2020-01-09 11:24:05 +0100

    added and removed areas for worktime

--- app/models/work_time.rb
+++ app/models/work_time.rb
@@ -3,24 +3,30 @@ class WorkTime < ApplicationRecord
 
   def self.areas
     [
+      "Institute",
+      "Entwicklung Inhalt (Blogs)",
+      "Entwicklung Verein",
+      "Entwicklung Netzwerk",
+      "Festival",
+      "Mitgliedschaft",
       "Basisbetrieb Inhalt",
       "Basisbetrieb Netzwerk",
       "Basisbetrieb Technik",
       "Fundraising",
       "Rechnungswesen",
       "overhead",
-      "Interviews",
-      "Fachferne",
-      "Blog affin (forschungsprojekte, volksabstimmungen, aktivitäten)",
+      #"Interviews",
+      #{}"Fachferne",
+      #{}"Blog affin (forschungsprojekte, volksabstimmungen, aktivitäten)",
       "Memento",
-      "SPG Technik",
-      "Essays Geschichte",
-      "Lernspiel",
+      #{}"SPG Technik",
+      #{}"Essays Geschichte",
+      #{}"Lernspiel",
       "Vorstand",
       "Events",
-      "Kurse",
+      #{}"Kurse",
       "Produkte",
-      "webmastering"
+      #{}"webmastering"
     ]
   end
 
