1a084958fec340d828503480ff68671dc9047986
Metaphysiker <s.raess@me.com>
2020-11-17 14:54:48 +0100

    changed some files

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -159,7 +159,8 @@ class PeopleController < ApplicationController
     end
     @records = Search.new(@search_inputs).search
 
-    report = ODFReport::Report.new("#{Rails.root}/app/views/odfs/serienbrief.odt") do |r|
+    report = ODFReport::Report.new("#{Rails.root}/app/views/odfs/kalender-spender.odt") do |r|
+    #report = ODFReport::Report.new("#{Rails.root}/app/views/odfs/serienbrief.odt") do |r|
        #r.add_field :address, @institution.address.address_for_letter
        #r.add_field :date, I18n.localize(Date.today, format: '%d.%B %Y').to_s
 
